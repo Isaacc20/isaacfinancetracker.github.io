0480b96a3f2e7b1e6ccf6b1eca9f4b74c4c550b5
Isaacc20 <isaacoluwadarasimi002@gmail.com>
2025-11-07 14:57:35 +0100

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Router, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Router>
+      {/* <Router> */}
         <Routes>
-        
+        <Route path='/' element={<App/>} >
+        <Route path='/add-transaction' element={} />
+        </Route>
         </Routes>
-      </Router>
+      {/* </Router> */}
     </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/Add.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <>
+        <form action="">
+            <input type="date" placeholder='enter date' />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+        </form>
+    </>
+  )
+}
+
+export default Add
